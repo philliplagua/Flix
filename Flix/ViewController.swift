9e10df6ae79f6d207a535715f68b514a4a392208
Phillip Lagua <philliplagua@gmail.com>
2020-08-31 17:04:30 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Phillip Lagua on 8/31/20.
+//  Copyright © 2020 Phillip Lagua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
