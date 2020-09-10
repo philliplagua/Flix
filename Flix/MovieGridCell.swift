6c31144897afcfb9873bf1b2d070b6dd4850161f
Phillip Lagua <philliplagua@gmail.com>
2020-09-09 19:31:13 -0700

    Tab bar with Movies and Superhero views

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Phillip Lagua on 9/9/20.
+//  Copyright © 2020 Phillip Lagua. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
